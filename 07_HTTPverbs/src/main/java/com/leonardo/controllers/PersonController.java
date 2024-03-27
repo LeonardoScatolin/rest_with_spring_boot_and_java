9814e4cdacd41cbcc02fea315dfa1c505b5f5b50
LeonardoScatolin <leonardo.scatolin20@gmail.com>
2024-03-27 15:06:47 -0300

    Working with verbs

---
+++ 07_HTTPverbs/src/main/java/com/leonardo/controllers/PersonController.java
@@ -0,0 +1,43 @@
+package com.leonardo.controllers;
+
+import com.leonardo.model.Person;
+import com.leonardo.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonServices service;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable String id) {
+        return service.findById(id);
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
+        return service.update(person);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void Delete(@PathVariable String id) {
+        service.delete(id);
+    }
+
+}
